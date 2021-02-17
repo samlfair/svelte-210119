c5768045ddd89cbd72aec608124319b3f128fe18
Sam Littlefair <sam.littlefair@prismic.io>
2021-02-17 14:34:09 +0100

    Get params

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,19 @@
+<script>
+  // import Header from "$components/Header.svelte";
+  // import Footer from "$components/Footer.svelte";
+</script>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+</style>
+
+<main>
+  <p>Header</p>
+  <!-- <Header /> -->
+  <slot />
+  <!-- <Footer /> -->
+  <p>Footer</p>
+</main>
---
+++ src/routes/[uid].svelte
@@ -0,0 +1,16 @@
+<script context="module">
+  import {Client} from '../../prismic-config'
+    export async function load({page}) {
+      document = await Client.getSingle('about')
+      const { params } = page
+        return { props: { params: params, document: document } } 
+    }
+</script>
+
+<script>
+  export let document
+</script>
+
+
+  <pre>{document}</pre>
+
